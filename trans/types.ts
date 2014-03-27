ff987429545fa429f4198397def67a1eb2a19880
Vlad Vergu <v.vergu@gmail.com>
2014-03-27 11:03:35 +0100

    Add hack-ish manual pushing of expected-type into subterms of Con

--- trans/types.ts
+++ trans/types.ts
@@ -1,12 +1,23 @@
 module types
 
+imports
+  lib/types/-
+
 type rules
 
+  Var(x): ty
+  where definition of x : ty
+
   VarRef(x) : ty
+  where definition of x : ty
+  
+  TypedVar(x, _) : ty
   where definition of x : ty
   
   Con(c, t*) : ty
   where definition of c : (ty*, ty)
     and t* : ty_t*
-    and ty* == ty_t* 
+    and ty* == ty_t*
         else error "types of sub-terms do not match constructor definition" on c
+
+
