bf449a3690ef646f8de226f86daa4faf859b12f6
Vlad Vergu <v.vergu@gmail.com>
2014-09-02 13:15:00 +0300

    Add custom exception on interpreter exit

---
+++ org.metaborg.meta.interpreter.framework/src/org/metaborg/meta/interpreter/framework/InterpreterExitException.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.metaborg.meta.interpreter.framework;
+
+/**
+ * @author vladvergu
+ *
+ */
+public class InterpreterExitException extends InterpreterException {
+
+	public InterpreterExitException(String string) {
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1056463372291035421L;
+
+}
