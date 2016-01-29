edf9c23c23d6c0f6a49d9d125a7b07daf047bdce
Vlad Vergu <v.vergu@gmail.com>
2016-01-29 15:27:20 +0200

    Specify an example Types class to serve as source for the generation of type system for Truffle

---
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/metaborg/meta/lang/dynsem/interpreter/terms/languagespecific/Types.java
@@ -0,0 +1,8 @@
+package metaborg.meta.lang.dynsem.interpreter.terms.languagespecific;
+
+import com.oracle.truffle.api.dsl.TypeSystem;
+
+@TypeSystem({ PlusTerm.class, IExprTerm.class })
+public class Types {
+
+}
