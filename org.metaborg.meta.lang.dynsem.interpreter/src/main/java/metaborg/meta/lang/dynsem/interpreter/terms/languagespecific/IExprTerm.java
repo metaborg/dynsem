0b25408c4806a9ecef4806695a68dcdc09730874
Vlad Vergu <v.vergu@gmail.com>
2016-02-01 18:32:25 +0200

    Split the calclang specific constructors from the dynsem interpreter

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/metaborg/meta/lang/dynsem/interpreter/terms/languagespecific/IExprTerm.java
+++
@@ -1,7 +0,0 @@
-package metaborg.meta.lang.dynsem.interpreter.terms.languagespecific;
-
-import metaborg.meta.lang.dynsem.interpreter.terms.IConTerm;
-
-public interface IExprTerm extends IConTerm {
-
-}
--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/metaborg/meta/lang/dynsem/interpreter/terms/languagespecific/Types.java
+++
@@ -1,8 +0,0 @@
-package metaborg.meta.lang.dynsem.interpreter.terms.languagespecific;
-
-import com.oracle.truffle.api.dsl.TypeSystem;
-
-@TypeSystem({ PlusTerm.class, IExprTerm.class })
-public class Types {
-
-}
