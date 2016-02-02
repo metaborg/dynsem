95fa0865a890f31414050017ed3869cb8271805c
Vlad Vergu <v.vergu@gmail.com>
2016-02-02 18:33:58 +0200

    New interface for a registry of language specific terms

---
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/ITermRegistry.java
@@ -0,0 +1,10 @@
+package org.metaborg.meta.lang.dynsem.interpreter;
+
+import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.ITermBuildFactory;
+import org.metaborg.meta.lang.dynsem.interpreter.nodes.matching.ITermMatchPatternFactory;
+
+public interface ITermRegistry {
+	public ITermBuildFactory getBuildFactory(String constr, int arity);
+
+	public ITermMatchPatternFactory getMatcherFactory(String constr, int arity);
+}
