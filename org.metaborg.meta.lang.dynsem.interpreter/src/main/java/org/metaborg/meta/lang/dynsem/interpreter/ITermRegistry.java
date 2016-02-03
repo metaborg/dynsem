71f66eaa608ced0bdb266b4cb4707c4cf746915e
Vlad Vergu <v.vergu@gmail.com>
2016-02-03 21:58:19 +0200

    TermRegistry should provie a method for converting an AST of the object language into an instantiation of the language-specific term structure.

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/ITermRegistry.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/ITermRegistry.java
@@ -1,11 +1,16 @@
 package org.metaborg.meta.lang.dynsem.interpreter;
 
+import metaborg.meta.lang.dynsem.interpreter.terms.ITerm;
+
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.ITermBuildFactory;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.matching.ITermMatchPatternFactory;
+import org.spoofax.interpreter.terms.IStrategoTerm;
 
 public interface ITermRegistry {
 	
 	public ITermBuildFactory lookupBuildFactory(String constr, int arity);
 
 	public ITermMatchPatternFactory lookupMatchFactory(String name, int arity);
+	
+	public ITerm parseFromStrategoTerm(IStrategoTerm t);
 }
