02c2263954e076804dac8126bff52e63e0618c09
Vlad Vergu <v.vergu@gmail.com>
2016-02-03 21:04:14 +0200

    Test suite! Test for loading a specifiation aterm

---
+++ org.metaborg.meta.lang.dynsem.interpreter/src/test/java/org/metaborg/meta/lang/dynsem/interpreter/test/TestSpecLoad.java
@@ -0,0 +1,19 @@
+package org.metaborg.meta.lang.dynsem.interpreter.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.metaborg.meta.lang.dynsem.interpreter.RuleRegistry;
+
+public class TestSpecLoad {
+
+	@Test
+	public void testLoadSpec() throws Exception {
+		File testDir = new File("src/test/resources/");
+		File specFile = new File(testDir, "testSpec1.aterm");
+		RuleRegistry rreg = RuleRegistry.create(specFile);
+		assertEquals(5, rreg.ruleCount());
+	}
+}
---
+++ org.metaborg.meta.lang.dynsem.interpreter/src/test/java/org/metaborg/meta/lang/dynsem/interpreter/test/TestSuite.java
@@ -0,0 +1,10 @@
+package org.metaborg.meta.lang.dynsem.interpreter.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ TestSpecLoad.class })
+public class TestSuite {
+
+}
