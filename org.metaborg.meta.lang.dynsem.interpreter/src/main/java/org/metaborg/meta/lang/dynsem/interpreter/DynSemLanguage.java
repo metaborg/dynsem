dfe8207147540fb33ab86cb1af0eea7c6be0e77f
Vlad Vergu <v.vergu@gmail.com>
2016-02-04 13:20:24 +0200

    Implementation DynSemLanguage class.

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/DynSemLanguage.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/DynSemLanguage.java
@@ -2,11 +2,18 @@ package org.metaborg.meta.lang.dynsem.interpreter;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+import org.spoofax.terms.util.NotImplementedException;
+
 import com.oracle.truffle.api.TruffleLanguage;
+import com.oracle.truffle.api.frame.MaterializedFrame;
+import com.oracle.truffle.api.instrument.Visualizer;
+import com.oracle.truffle.api.instrument.WrapperNode;
 import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.source.Source;
 
 public abstract class DynSemLanguage extends TruffleLanguage<DynSemContext> {
 
@@ -31,12 +38,49 @@ public abstract class DynSemLanguage extends TruffleLanguage<DynSemContext> {
 		return context;
 	}
 
+	@Override
+	protected WrapperNode createWrapperNode(Node node) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	protected Object evalInContext(Source source, Node node,
+			MaterializedFrame mFrame) throws IOException {
+		throw new IllegalStateException("evalInContext not supported");
+	}
+
 	public Node createFindContextNode0() {
 		return createFindContextNode();
 	}
 
 	public DynSemContext findContext0(Node n) {
 		return findContext(n);
+	}
+
+	@Override
+	protected Object findExportedSymbol(DynSemContext context,
+			String globalName, boolean onlyExplicit) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	protected boolean isObjectOfLanguage(Object object) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	protected Object getLanguageGlobal(DynSemContext context) {
+		return context;
+	}
+
+	@Override
+	protected Visualizer getVisualizer() {
+		return null;
+	}
+
+	@Override
+	protected boolean isInstrumentable(Node node) {
+		return false;
 	}
 
 }
