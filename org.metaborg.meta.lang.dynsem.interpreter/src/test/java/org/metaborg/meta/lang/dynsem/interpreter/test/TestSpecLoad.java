486ebdb111db70fde266e81aa61c5b2c9fff20b0
Vlad Vergu <v.vergu@gmail.com>
2016-02-26 19:34:20 +0200

    Update entrypoint generator and bring specification loading test suite up to date.

--- org.metaborg.meta.lang.dynsem.interpreter/src/test/java/org/metaborg/meta/lang/dynsem/interpreter/test/TestSpecLoad.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/test/java/org/metaborg/meta/lang/dynsem/interpreter/test/TestSpecLoad.java
@@ -3,9 +3,19 @@ package org.metaborg.meta.lang.dynsem.interpreter.test;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 
 import org.junit.Test;
+import org.metaborg.meta.lang.dynsem.interpreter.DynSemContext;
+import org.metaborg.meta.lang.dynsem.interpreter.DynSemLanguage;
 import org.metaborg.meta.lang.dynsem.interpreter.RuleRegistry;
+
+import com.oracle.truffle.api.CallTarget;
+import com.oracle.truffle.api.TruffleLanguage;
+import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.source.Source;
 
 public class TestSpecLoad {
 
@@ -13,10 +23,36 @@ public class TestSpecLoad {
 	public void testLoadSpec() throws Exception {
 		File testDir = new File("src/test/resources/");
 		File specFile = new File(testDir, "testSpec1.aterm");
+
 		RuleRegistry rreg = new RuleRegistry() {
 		};
+
+		assert (DummyDynSemLanguage.INSTANCE != null);
+
 		RuleRegistry.populate(rreg, specFile);
 		assertEquals(5, rreg.ruleCount());
-//		NodeUtil.printTreeToString(rreg.lookupRule("Plus", 2));
+	}
+
+	@TruffleLanguage.Registration(name = "Dummy", version = "0.1", mimeType = "application/x-dummy")
+	public static class DummyDynSemLanguage extends DynSemLanguage {
+
+		public static final DummyDynSemLanguage INSTANCE = new DummyDynSemLanguage();
+
+		public DummyDynSemLanguage() {
+			DynSemContext.LANGUAGE = this;
+		}
+
+		@Override
+		public DynSemContext createDynSemContext(InputStream input,
+				PrintStream output) {
+			return new DynSemContext(null, null, null);
+		}
+
+		@Override
+		protected CallTarget parse(Source code, Node context,
+				String... argumentNames) throws IOException {
+			return null;
+		}
+
 	}
 }
