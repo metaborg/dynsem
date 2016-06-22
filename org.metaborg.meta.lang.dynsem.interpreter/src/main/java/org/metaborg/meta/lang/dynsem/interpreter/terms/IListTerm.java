10ee4caa6bed5cbd3569f0c5033405bbb569810b
Vlad Vergu <v.vergu@gmail.com>
2016-06-22 19:00:28 +0300

    Extend the IListTerm interface and take/Int and drop/Int convenience methods.

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/terms/IListTerm.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/terms/IListTerm.java
@@ -6,12 +6,16 @@ public interface IListTerm<T> extends ITerm {
 
 	public T head();
 
+	public T[] take(int numElems);
+
 	public IListTerm<T> tail();
+	
+	public IListTerm<T> drop(int numElems);
 
 	public IListTerm<T> add(T elem);
-	
+
 	public IListTerm<T> addAll(T[] elems);
-	
+
 	public Iterator<T> iterator();
 
 }
