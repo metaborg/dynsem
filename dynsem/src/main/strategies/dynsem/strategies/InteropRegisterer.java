ac30a36fd1fd347758d5413e80358609d2301b9c
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2016-09-17 11:29:43 +0200

    Make generator more robust for URI paths.
    * Use absolute-path strategy from libspoofax.
    * Remove ds-rmdir, since it has same semantics as rmdir.
    * Drop mkdir(recursive), since mkdir already creates missing parents.

--- dynsem/src/main/strategies/dynsem/strategies/InteropRegisterer.java
+++ dynsem/src/main/strategies/dynsem/strategies/InteropRegisterer.java
@@ -5,6 +5,6 @@ import org.strategoxt.lang.Strategy;
 
 public class InteropRegisterer extends JavaInteropRegisterer {
     public InteropRegisterer() {
-        super(new Strategy[] { read_property_0_2.instance, make_absolute_path_0_1.instance, ds_rmdir_0_0.instance });
+        super(new Strategy[] { read_property_0_2.instance });
     }
 }
