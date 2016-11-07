a812255d73aba6ba71c42f8f669357c82dc52038
Vlad Vergu <v.vergu@gmail.com>
2016-11-07 18:30:55 +0100

    Alright, we're getting somewhere now with the Travis CI build of DynSem and dynsem2coq

--- travis-build.sh
+++ travis-build.sh
@@ -4,5 +4,5 @@ cd $TRAVIS_BUILD_DIR/dynsem
 mvn -Pstandalone install
 cd $TRAVIS_BUILD_DIR/dynsem2coq
 mvn -Pstandalone install
-cd org.metaborg.meta.lang.dynsem.interpreter
+cd $TRAVIS_BUILD_DIR/org.metaborg.meta.lang.dynsem.interpreter
 mvn -Pstandalone test
