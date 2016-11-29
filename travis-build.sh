e9710a037809a1db37c24c0386db1e7d9d1cdadb
Vlad Vergu <v.vergu@gmail.com>
2016-11-29 10:44:13 +0200

    Fix incorrect interpreter path in travis build script

--- travis-build.sh
+++ travis-build.sh
@@ -8,5 +8,5 @@ cd $TRAVIS_BUILD_DIR/org.metaborg.meta.lang.dynsem.interpreter
 mvn -Pstandalone test
 cd $TRAVIS_BUILD_DIR/metaborg-sl/org.metaborg.lang.sl
 mvn -Pstandalone test
-cd $TRAVIS_BUILD_DIR/metaborg-sl/org.metaborg.lang.sl.interpreter
+cd $TRAVIS_BUILD_DIR/metaborg-sl/org.metaborg.lang.sl.interp
 mvn -Pstandalone test
