3d80ad4ba68e0a29c240112198fbe416251d3a2c
Vlad Vergu <v.vergu@gmail.com>
2016-12-07 17:38:13 +0100

    Re-enable caching during the build

--- travis-build.sh
+++ travis-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ev
 cd $TRAVIS_BUILD_DIR/dynsem
-mvn -U -Pstandalone install
+mvn -Pstandalone install
 cd $TRAVIS_BUILD_DIR/dynsem2coq
-mvn -U -Pstandalone install
+mvn -Pstandalone install
 cd $TRAVIS_BUILD_DIR/org.metaborg.meta.lang.dynsem.interpreter
-mvn -U -Pstandalone install
+mvn -Pstandalone install
 cd $TRAVIS_BUILD_DIR/metaborg-sl/org.metaborg.lang.sl
-mvn -U -Pstandalone install
+mvn -Pstandalone install
 cd $TRAVIS_BUILD_DIR/metaborg-sl/org.metaborg.lang.sl.interp
-mvn -U -Pstandalone install
+mvn -Pstandalone install
