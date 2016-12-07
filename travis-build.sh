4530f8bbbc8345226e62c47dfe58f0e424ff40c8
Vlad Vergu <v.vergu@gmail.com>
2016-12-07 17:31:22 +0100

    Actually install the DynSem specification interpreter during the Travis build

--- travis-build.sh
+++ travis-build.sh
@@ -5,8 +5,8 @@ mvn -U -Pstandalone install
 cd $TRAVIS_BUILD_DIR/dynsem2coq
 mvn -U -Pstandalone install
 cd $TRAVIS_BUILD_DIR/org.metaborg.meta.lang.dynsem.interpreter
-mvn -U -Pstandalone test
+mvn -U -Pstandalone install
 cd $TRAVIS_BUILD_DIR/metaborg-sl/org.metaborg.lang.sl
-mvn -U -Pstandalone test
+mvn -U -Pstandalone install
 cd $TRAVIS_BUILD_DIR/metaborg-sl/org.metaborg.lang.sl.interp
-mvn -U -Pstandalone test
+mvn -U -Pstandalone install
