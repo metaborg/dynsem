90173ac418dd3b55543e2ffa5456a5fb9ea7ed4f
Vlad Vergu <v.vergu@gmail.com>
2016-12-07 16:51:03 +0100

    Travis build is failing strangely, disabling caching

--- travis-build.sh
+++ travis-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ev
 cd $TRAVIS_BUILD_DIR/dynsem
-mvn -Pstandalone install
+mvn -U -Pstandalone install
 cd $TRAVIS_BUILD_DIR/dynsem2coq
-mvn -Pstandalone install
+mvn -U -Pstandalone install
 cd $TRAVIS_BUILD_DIR/org.metaborg.meta.lang.dynsem.interpreter
-mvn -Pstandalone test
+mvn -U -Pstandalone test
 cd $TRAVIS_BUILD_DIR/metaborg-sl/org.metaborg.lang.sl
-mvn -Pstandalone test
+mvn -U -Pstandalone test
 cd $TRAVIS_BUILD_DIR/metaborg-sl/org.metaborg.lang.sl.interp
-mvn -Pstandalone test
+mvn -U -Pstandalone test
