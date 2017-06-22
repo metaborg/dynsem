acacdcde27803c499b666d1715d605b0028ecda1
Vlad Vergu <v.vergu@gmail.com>
2017-06-22 10:21:56 +0300

    Fixes interpreter using "Map" instead of "Map_" constructor

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nodes/building/TermBuild.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nodes/building/TermBuild.java
@@ -80,7 +80,7 @@ public abstract class TermBuild extends DynSemNode {
 		if (Tools.hasConstructor(t, "ArgRead", 1)) {
 			return ArgRead.create(t);
 		}
-		if (Tools.hasConstructor(t, "Map", 1)) {
+		if (Tools.hasConstructor(t, "Map_", 1)) {
 			return MapBuild.create(t, fd);
 		}
 		if (Tools.hasConstructor(t, "MapExtend", 2)) {
