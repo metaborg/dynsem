2c043c536220ea73b953a2fa9e77997eda048f60
Vlad Vergu <v.vergu@gmail.com>
2017-06-28 18:13:31 +0300

    Disables building of the dynsem meta-meta interpreter on Travis CI

--- travis-build.sh
+++ travis-build.sh
@@ -6,8 +6,8 @@ cd $TRAVIS_BUILD_DIR/dynsem2coq
 mvn -Pstandalone install
 cd $TRAVIS_BUILD_DIR/org.metaborg.meta.lang.dynsem.interpreter
 mvn -Pstandalone install
-cd $TRAVIS_BUILD_DIR/dynsem.metainterpreter
-mvn -Pstandalone install
+# cd $TRAVIS_BUILD_DIR/dynsem.metainterpreter
+# mvn -Pstandalone install
 cd $TRAVIS_BUILD_DIR/metaborg-sl/org.metaborg.lang.sl
 mvn -Pstandalone install
 cd $TRAVIS_BUILD_DIR/metaborg-sl/org.metaborg.lang.sl.interp
