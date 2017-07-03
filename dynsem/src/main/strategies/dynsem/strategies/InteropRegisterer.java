87637f566b19e4d82186cf52018a72f2808c697f
Vlad Vergu <v.vergu@gmail.com>
2017-07-03 15:45:32 +0300

    Adds skipping of Java class generation if signatures are unchanged

--- dynsem/src/main/strategies/dynsem/strategies/InteropRegisterer.java
+++ dynsem/src/main/strategies/dynsem/strategies/InteropRegisterer.java
@@ -4,7 +4,8 @@ import org.strategoxt.lang.JavaInteropRegisterer;
 import org.strategoxt.lang.Strategy;
 
 public class InteropRegisterer extends JavaInteropRegisterer {
-    public InteropRegisterer() {
-        super(new Strategy[] { read_property_0_2.instance, fmakeexecutable_0_0.instance, native_name_matches_varscheme_0_1.instance });
-    }
+	public InteropRegisterer() {
+		super(new Strategy[] { read_property_0_2.instance, fmakeexecutable_0_0.instance,
+				native_name_matches_varscheme_0_1.instance, digest_term_0_0.instance });
+	}
 }
