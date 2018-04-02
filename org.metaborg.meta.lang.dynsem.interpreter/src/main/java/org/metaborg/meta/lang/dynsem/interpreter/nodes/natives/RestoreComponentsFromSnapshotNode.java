543d0ff2c991325f34f08fd3eca7f2f4622e9795
Vlad Vergu <v.vergu@gmail.com>
2018-04-02 11:47:21 +0300

    Fuse component snapshoting/restoring classes

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nodes/natives/RestoreComponentsFromSnapshotNode.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nodes/natives/RestoreComponentsFromSnapshotNode.java
@@ -1,13 +1,16 @@
 package org.metaborg.meta.lang.dynsem.interpreter.nodes.natives;
 
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.DynSemNode;
+import org.metaborg.meta.lang.dynsem.interpreter.nodes.matching.MatchPattern;
 import org.metaborg.meta.lang.dynsem.interpreter.utils.SourceUtils;
+import org.spoofax.interpreter.core.Tools;
 import org.spoofax.interpreter.terms.IStrategoAppl;
 import org.spoofax.interpreter.terms.IStrategoList;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 
 import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.frame.FrameDescriptor;
+import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
 import com.oracle.truffle.api.source.SourceSection;
@@ -40,5 +43,31 @@ public class RestoreComponentsFromSnapshotNode extends DynSemNode {
 		}
 		return new RestoreComponentsFromSnapshotNode(SourceUtils.dynsemSourceSectionFromATerm(labelComps),
 				captureNodes);
+	}
+	
+	public static class RestoreComponentFromSnapshotNode extends DynSemNode {
+
+		private final FrameSlot componentSlot;
+		@Child private MatchPattern patt;
+
+		public RestoreComponentFromSnapshotNode(SourceSection source, FrameSlot compSlot, MatchPattern patt) {
+			super(source);
+			this.componentSlot = compSlot;
+			this.patt = patt;
+		}
+
+		public void executeRestore(VirtualFrame frame, VirtualFrame components) {
+			patt.executeMatch(frame, components.getValue(componentSlot));
+		}
+
+		
+		public static RestoreComponentFromSnapshotNode create(IStrategoAppl labelComp, FrameDescriptor ruleFD, FrameDescriptor componentsFD) {
+			CompilerAsserts.neverPartOfCompilation();
+			assert Tools.hasConstructor(labelComp, "LabelComp", 2);
+			MatchPattern patt = MatchPattern.createFromLabelComp(labelComp, ruleFD);
+			FrameSlot slot = componentsFD.findFrameSlot(Tools.javaStringAt(Tools.applAt(labelComp, 0), 0));
+			assert slot != null;
+			return new RestoreComponentFromSnapshotNode(SourceUtils.dynsemSourceSectionFromATerm(labelComp), slot, patt);
+		}
 	}
 }
--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nodes/natives/SnapshotComponentsNode.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nodes/natives/SnapshotComponentsNode.java
@@ -1,7 +1,9 @@
 package org.metaborg.meta.lang.dynsem.interpreter.nodes.natives;
 
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.DynSemNode;
+import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.TermBuild;
 import org.metaborg.meta.lang.dynsem.interpreter.utils.SourceUtils;
+import org.spoofax.interpreter.core.Tools;
 import org.spoofax.interpreter.terms.IStrategoAppl;
 import org.spoofax.interpreter.terms.IStrategoList;
 import org.spoofax.interpreter.terms.IStrategoTerm;
@@ -9,6 +11,7 @@ import org.spoofax.interpreter.terms.IStrategoTerm;
 import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.FrameDescriptor;
+import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
 import com.oracle.truffle.api.source.SourceSection;
@@ -48,5 +51,32 @@ public class SnapshotComponentsNode extends DynSemNode {
 		}
 		return new SnapshotComponentsNode(SourceUtils.dynsemSourceSectionFromATerm(labelComps), componentsFD,
 				captureNodes);
+	}
+	
+	public static class CaptureComponentNode extends DynSemNode {
+
+		@Child private TermBuild tb;
+		
+		private final FrameSlot componentSlot;
+
+		public CaptureComponentNode(SourceSection source, FrameSlot compSlot, TermBuild tb) {
+			super(source);
+			this.componentSlot = compSlot;
+			this.tb = tb;
+		}
+
+		public void executeCapture(VirtualFrame frame, VirtualFrame components) {
+			components.setObject(componentSlot, tb.executeGeneric(frame));
+		}
+		
+		public static CaptureComponentNode create(IStrategoAppl labelComp, FrameDescriptor ruleFD, FrameDescriptor componentsFD) {
+			CompilerAsserts.neverPartOfCompilation();
+			assert Tools.hasConstructor(labelComp, "LabelComp", 2);
+			TermBuild tb = TermBuild.createFromLabelComp(labelComp, ruleFD);
+			FrameSlot slot = componentsFD.findFrameSlot(Tools.javaStringAt(Tools.applAt(labelComp, 0), 0));
+			assert slot != null;
+			return new CaptureComponentNode(SourceUtils.dynsemSourceSectionFromATerm(labelComp), slot, tb);
+		}
+
 	}
 }
