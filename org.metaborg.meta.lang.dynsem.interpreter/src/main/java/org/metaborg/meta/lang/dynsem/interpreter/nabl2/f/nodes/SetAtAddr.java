caa8adb5b91f569a96e3b1b0906de8cf23baefd8
Vlad Vergu <v.vergu@gmail.com>
2018-04-19 17:34:50 +0300

    Implement setting value at address for frames and arrays

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nabl2/f/nodes/SetAtAddr.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nabl2/f/nodes/SetAtAddr.java
@@ -1,13 +1,15 @@
 package org.metaborg.meta.lang.dynsem.interpreter.nabl2.f.nodes;
 
-import org.metaborg.meta.lang.dynsem.interpreter.nabl2.f.Addr;
+import org.metaborg.meta.lang.dynsem.interpreter.nabl2.f.FrameAddr;
+import org.metaborg.meta.lang.dynsem.interpreter.nabl2.f.arrays.ArrayAddr;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.NativeOpBuild;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.TermBuild;
-import org.metaborg.meta.lang.dynsem.interpreter.terms.shared.ValSort;
 
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.NodeChildren;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.object.FinalLocationException;
+import com.oracle.truffle.api.object.IncompatibleLocationException;
 import com.oracle.truffle.api.source.SourceSection;
 
 @NodeChildren({ @NodeChild(value = "addr", type = TermBuild.class), @NodeChild(value = "val", type = TermBuild.class) })
@@ -16,12 +18,20 @@ public abstract class SetAtAddr extends NativeOpBuild {
 	public SetAtAddr(SourceSection source) {
 		super(source);
 	}
-
-	// TODO: 2 cases: arrayaddr and frameaddr
+	@Specialization
+	public Object executeFrameSet(FrameAddr addr, Object val) {
+		try {
+			addr.location().set(addr.frame(), val);
+		} catch (IncompatibleLocationException | FinalLocationException e) {
+			throw new IllegalStateException(e);
+		}
+		return val;
+	}
 
 	@Specialization
-	public ValSort executeGet(Addr addr, ValSort val) {
-		throw new IllegalStateException("Set not implemented");
+	public Object executeArraySet(ArrayAddr addr, Object val) {
+		addr.arr().set(addr.idx(), val);
+		return val;
 	}
 
 	public static SetAtAddr create(SourceSection source, TermBuild addr, TermBuild val) {
