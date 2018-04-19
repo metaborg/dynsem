9832f1a659fd971378ba61d95bb91563f4eba87c
Vlad Vergu <v.vergu@gmail.com>
2018-04-19 10:46:07 +0300

    Massages Java's generics to allow more informative list types to be returned (helps with usage, making casts faster and simpler)

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/ITermRegistry.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/ITermRegistry.java
@@ -2,6 +2,7 @@ package org.metaborg.meta.lang.dynsem.interpreter;
 
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.ITermBuildFactory;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.matching.ITermMatchPatternFactory;
+import org.metaborg.meta.lang.dynsem.interpreter.terms.IListTerm;
 import org.metaborg.meta.lang.dynsem.interpreter.terms.ITerm;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 
@@ -19,7 +20,7 @@ public interface ITermRegistry {
 
 	public Class<?> getNativeOperatorClass(String constr, int arity);
 
-	public Class<?> getListClass(Class<?> elemClass);
+	public <T> Class<? extends IListTerm<T>> getListClass(Class<T> elemClass);
 
 	public Class<?> getMapClass(String keySortName, String mapSortName);
 
