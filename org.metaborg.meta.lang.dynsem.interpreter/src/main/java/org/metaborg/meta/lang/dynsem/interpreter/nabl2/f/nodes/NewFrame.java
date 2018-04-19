a418ef9dc123be3524d184d09474c2182d84a12d
Vlad Vergu <v.vergu@gmail.com>
2018-04-19 14:49:04 +0300

    Improves NewFrame native operator by caching the protoframe returned from the context

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nabl2/f/nodes/NewFrame.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nabl2/f/nodes/NewFrame.java
@@ -1,6 +1,5 @@
 package org.metaborg.meta.lang.dynsem.interpreter.nabl2.f.nodes;
 
-import org.metaborg.meta.lang.dynsem.interpreter.DynSemContext;
 import org.metaborg.meta.lang.dynsem.interpreter.nabl2.f.FrameLink;
 import org.metaborg.meta.lang.dynsem.interpreter.nabl2.f.layouts.FrameLayoutImpl;
 import org.metaborg.meta.lang.dynsem.interpreter.nabl2.sg.ScopeIdentifier;
@@ -8,6 +7,8 @@ import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.NativeOpBuild;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.TermBuild;
 import org.metaborg.meta.lang.dynsem.interpreter.terms.IListTerm;
 
+import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.NodeChildren;
 import com.oracle.truffle.api.dsl.Specialization;
@@ -22,21 +23,39 @@ public abstract class NewFrame extends NativeOpBuild {
 		super(source);
 	}
 
-	@Specialization
-	public DynamicObject executeCreateFrame(ScopeIdentifier scopeident, IListTerm<?> links) {
-		DynSemContext ctx = getContext();
+	@Specialization(guards = { "scopeident == scopeident_cached" })
+	public DynamicObject createCached(ScopeIdentifier scopeident, Object links,
+			@Cached("lookupListClass()") Class<? extends IListTerm<FrameLink>> linksListClass,
+			@Cached("scopeident") ScopeIdentifier scopeident_cached,
+			@Cached("lookupProtoFrame(scopeident)") DynamicObject protoFrame) {
+		return actualCreate(linksListClass.cast(links), protoFrame);
+	}
 
-		IListTerm<FrameLink> actualLinks = ctx.getTermRegistry().getListClass(FrameLink.class).cast(links);
+	@Specialization(replaces = "createCached")
+	public DynamicObject executeCreateFrame(ScopeIdentifier scopeident, Object links,
+			@Cached("lookupListClass()") Class<? extends IListTerm<FrameLink>> linksListClass) {
+		return actualCreate(linksListClass.cast(links), getContext().getProtoFrame(scopeident));
+	}
 
-		DynamicObject protoFrame = ctx.getProtoFrame(scopeident);
+	private DynamicObject actualCreate(IListTerm<FrameLink> links, DynamicObject protoFrame) {
 		assert FrameLayoutImpl.INSTANCE.isFrame(protoFrame);
 
 		DynamicObject frame = protoFrame.copy(protoFrame.getShape());
-		for (FrameLink link : actualLinks) {
+		for (FrameLink link : links) {
 			frame.set(link.link(), link.frame());
 		}
 
 		return frame;
+	}
+
+	protected Class<? extends IListTerm<FrameLink>> lookupListClass() {
+		CompilerAsserts.neverPartOfCompilation();
+		return getContext().getTermRegistry().getListClass(FrameLink.class);
+	}
+
+	protected DynamicObject lookupProtoFrame(ScopeIdentifier scopeident) {
+		CompilerAsserts.neverPartOfCompilation();
+		return getContext().getProtoFrame(scopeident);
 	}
 
 	public static NewFrame create(SourceSection source, TermBuild scope, TermBuild links) {
