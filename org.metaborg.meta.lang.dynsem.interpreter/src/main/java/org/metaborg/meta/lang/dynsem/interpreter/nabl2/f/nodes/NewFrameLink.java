14ed938725e63407ae52453e9b517f023d746902
Vlad Vergu <v.vergu@gmail.com>
2018-04-19 17:34:28 +0300

    Adds faster implementation of frame link creation to increase sharing of FrameLinkIdentifier instances. This will give faster lookup times in hashtables

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nabl2/f/nodes/NewFrameLink.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nabl2/f/nodes/NewFrameLink.java
@@ -4,11 +4,13 @@ import org.metaborg.meta.lang.dynsem.interpreter.nabl2.f.FrameLink;
 import org.metaborg.meta.lang.dynsem.interpreter.nabl2.f.layouts.FrameLayoutImpl;
 import org.metaborg.meta.lang.dynsem.interpreter.nabl2.f.layouts.FrameLinkIdentifier;
 import org.metaborg.meta.lang.dynsem.interpreter.nabl2.sg.Label;
+import org.metaborg.meta.lang.dynsem.interpreter.nabl2.sg.ScopeIdentifier;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.NativeOpBuild;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.TermBuild;
 
 import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.NodeChildren;
 import com.oracle.truffle.api.dsl.Specialization;
@@ -18,22 +20,37 @@ import com.oracle.truffle.api.source.SourceSection;
 
 @NodeChildren({ @NodeChild(value = "label", type = TermBuild.class),
 		@NodeChild(value = "frm", type = TermBuild.class) })
+@ImportStatic(FrameLayoutImpl.class)
 public abstract class NewFrameLink extends NativeOpBuild {
 
 	public NewFrameLink(SourceSection source) {
 		super(source);
 	}
 
+	/*
+	 * we want to cache the FrameLinkIdentifier if the label and the linked scope is constant the linked scope is
+	 * constant if the shape of the frame is constant (because if two frames have the same shape then they have the same
+	 * scope)
+	 */
 	@Specialization(guards = { "label == label_cached", "shapeCheck(frm_shape, frm)" })
-	public FrameLink linkCached(Label label, DynamicObject frm, @Cached("label") Label label_cached,
+	public FrameLink createCached(Label label, DynamicObject frm, @Cached("label") Label label_cached,
 			@Cached("lookupShape(frm)") Shape frm_shape,
-			@Cached("createLinkIdentifier(label, null)") FrameLinkIdentifier linkIdent) {
+			@Cached("createLinkIdentifier(label, INSTANCE.getScope(frm))") FrameLinkIdentifier linkIdent) {
 		return new FrameLink(label, frm, linkIdent);
-		// return new FrameLink(label, frm, new FrameLinkIdentifier(label, FrameLayoutImpl.INSTANCE.getScope(frm)));
+	}
+
+	@Specialization(replaces = "createCached")
+	public FrameLink createUncached(Label label, DynamicObject frm) {
+		return new FrameLink(label, frm);
 	}
 
 	protected boolean shapeCheck(Shape shape, DynamicObject frm) {
 		return shape != null && shape.check(frm);
+	}
+
+	protected FrameLinkIdentifier createLinkIdentifier(Label label, ScopeIdentifier scopeIdent) {
+		CompilerAsserts.neverPartOfCompilation();
+		return new FrameLinkIdentifier(label, scopeIdent);
 	}
 
 	protected Shape lookupShape(DynamicObject frm) {
