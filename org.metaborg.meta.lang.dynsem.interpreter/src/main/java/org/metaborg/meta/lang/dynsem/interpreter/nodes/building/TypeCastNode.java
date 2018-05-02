70cd04345f672e1a4f3f4acb61f1f5809ee5d3d1
Vlad Vergu <v.vergu@gmail.com>
2018-05-02 18:56:59 +0300

    Revert "Rename specialization methods inside type cast nodes"
    This reverts commit 8ce707ff714f22038b46b7f7e3bd769c9c56e829.

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nodes/building/TypeCastNode.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nodes/building/TypeCastNode.java
@@ -1,18 +1,17 @@
 package org.metaborg.meta.lang.dynsem.interpreter.nodes.building;
 
-import org.metaborg.meta.lang.dynsem.interpreter.utils.SourceUtils;
-
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.source.SourceSection;
 
 @NodeChild(value = "tb", type = TermBuild.class)
 public abstract class TypeCastNode extends TermBuild {
 
-	public TypeCastNode() {
-		super(SourceUtils.dynsemSourceSectionUnvailable());
+	public TypeCastNode(SourceSection source) {
+		super(source);
 	}
 
-	public abstract Object executeCast(VirtualFrame frame);
+	public abstract Object executeLeafCast(VirtualFrame frame);
 
 	public abstract Object executeEvaluated(Object term);
 
