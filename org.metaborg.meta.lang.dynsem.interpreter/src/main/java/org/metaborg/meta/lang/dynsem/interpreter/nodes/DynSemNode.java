26afdb8a5cabaf60bbc21bc2e3d0eea28b7fec80
Vlad Vergu <v.vergu@gmail.com>
2018-05-28 10:22:53 +0300

    Add pure NaBL2 operations

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nodes/DynSemNode.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nodes/DynSemNode.java
@@ -1,13 +1,24 @@
 package org.metaborg.meta.lang.dynsem.interpreter.nodes;
 
+import static mb.nabl2.terms.build.TermBuild.B;
+
+import java.util.Optional;
+
 import org.metaborg.meta.lang.dynsem.interpreter.DynSemContext;
 import org.metaborg.meta.lang.dynsem.interpreter.DynSemLanguage;
 import org.metaborg.meta.lang.dynsem.interpreter.nabl2.NaBL2Context;
+import org.spoofax.interpreter.terms.IStrategoTerm;
 
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.source.SourceSection;
+
+import mb.nabl2.interpreter.InterpreterTerms;
+import mb.nabl2.stratego.ConstraintTerms;
+import mb.nabl2.stratego.StrategoTermIndices;
+import mb.nabl2.stratego.TermIndex;
+import mb.nabl2.terms.ITerm;
 
 public abstract class DynSemNode extends Node {
 	private final SourceSection sourceSection;
@@ -40,6 +51,35 @@ public abstract class DynSemNode extends Node {
 			context = (NaBL2Context) getContext().readProperty(NaBL2Context.class.getName(), null);
 		}
 		return context;
+	}
+
+	protected IStrategoTerm getSolution() {
+		return safeToStratego(InterpreterTerms.context(nabl2Context().getSolution()));
+	}
+
+	protected IStrategoTerm getAstProperty(IStrategoTerm sterm, String key) {
+		return getAstProperty(sterm, B.newAppl(key));
+	}
+
+	protected IStrategoTerm getAstProperty(IStrategoTerm sterm, ITerm key) {
+		TermIndex index = getTermIndex(sterm);
+		Optional<ITerm> val = nabl2Context().getSolution().astProperties().getValue(index, key);
+		if (!val.isPresent()) {
+			throw new IllegalArgumentException("Node has no " + key + " parameter");
+		}
+		return safeToStratego(val.get());
+	}
+
+	protected TermIndex getTermIndex(IStrategoTerm sterm) {
+		if (sterm == null) {
+			throw new IllegalArgumentException("Primitive must be called on an AST node.");
+		}
+		return StrategoTermIndices.get(sterm).orElseThrow(() -> new IllegalArgumentException("Node has no index."));
+	}
+
+	private IStrategoTerm safeToStratego(ITerm term) {
+		term = ConstraintTerms.explicate(term);
+		return nabl2Context().getStrategoTerms().toStratego(term);
 	}
 
 }
