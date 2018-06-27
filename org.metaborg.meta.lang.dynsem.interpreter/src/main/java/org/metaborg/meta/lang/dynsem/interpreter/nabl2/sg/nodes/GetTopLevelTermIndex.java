ad58e1c31d769e94140162308df721f7071622d2
Vlad Vergu <v.vergu@gmail.com>
2018-06-27 16:01:20 +0300

    Adds caching to top level term index node

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nabl2/sg/nodes/GetTopLevelTermIndex.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nabl2/sg/nodes/GetTopLevelTermIndex.java
@@ -6,6 +6,7 @@ import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.NativeOpBuild;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.TermBuild;
 import org.metaborg.meta.lang.dynsem.interpreter.terms.ITerm;
 
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.source.SourceSection;
@@ -17,8 +18,14 @@ public abstract class GetTopLevelTermIndex extends NativeOpBuild {
 		super(source);
 	}
 
+	@Specialization(guards = "term_cached == term", limit = "100")
+	public TermIndex doCached(ITerm term, @Cached("term") ITerm term_cached,
+			@Cached("doUncached(term_cached)") TermIndex index_cached) {
+		return index_cached;
+	}
+
 	@Specialization
-	public TermIndex executeTermIndex(ITerm term) {
+	public TermIndex doUncached(ITerm term) {
 		mb.nabl2.stratego.TermIndex termIndex = NaBL2SolutionUtils.getTermIndex(term.getStrategoTerm());
 
 		return new TermIndex(termIndex.getResource(), 0);
