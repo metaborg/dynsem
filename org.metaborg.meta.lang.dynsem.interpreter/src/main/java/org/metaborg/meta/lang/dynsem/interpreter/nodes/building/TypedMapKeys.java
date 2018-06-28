f624489f0b1b3e3a4644e1374fcd93a43bdf0ee5
Vlad Vergu <v.vergu@gmail.com>
2018-06-28 17:01:26 +0300

    Fixes bug related to getting all keys in a map in compiler backend and runtime

--- org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nodes/building/TypedMapKeys.java
+++ org.metaborg.meta.lang.dynsem.interpreter/src/main/java/org/metaborg/meta/lang/dynsem/interpreter/nodes/building/TypedMapKeys.java
@@ -1,17 +1,15 @@
 package org.metaborg.meta.lang.dynsem.interpreter.nodes.building;
 
-import java.lang.reflect.Constructor;
-import java.util.Collection;
+import java.lang.reflect.Array;
 
-import org.metaborg.meta.lang.dynsem.interpreter.InterpreterException;
-import org.metaborg.meta.lang.dynsem.interpreter.terms.IListTerm;
+import org.metaborg.meta.lang.dynsem.interpreter.ITermRegistry;
+import org.metaborg.meta.lang.dynsem.interpreter.terms.ITermInit;
 import org.metaborg.meta.lang.dynsem.interpreter.utils.SourceUtils;
 import org.spoofax.interpreter.core.Tools;
 import org.spoofax.interpreter.terms.IStrategoAppl;
 
 import com.github.krukow.clj_ds.PersistentMap;
 import com.oracle.truffle.api.CompilerAsserts;
-import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
@@ -22,55 +20,78 @@ import com.oracle.truffle.api.source.SourceSection;
 @NodeChild(value = "mapNode", type = TermBuild.class)
 public abstract class TypedMapKeys extends TermBuild {
 
-	private final String keyListClass;
+	private final String keyClass;
+	private final String keyArrayClass;
 
 	public static TypedMapKeys create(IStrategoAppl t, FrameDescriptor fd) {
 		CompilerAsserts.neverPartOfCompilation();
 		assert Tools.hasConstructor(t, "TypedMapKeys", 2);
 		TermBuild mapNode = TermBuild.create(Tools.applAt(t, 0), fd);
-		String keyListClass = Tools.javaStringAt(t, 1);
-		return TypedMapKeysNodeGen.create(SourceUtils.dynsemSourceSectionFromATerm(t), keyListClass, mapNode);
+		String keyClass = Tools.javaStringAt(t, 1);
+		return TypedMapKeysNodeGen.create(SourceUtils.dynsemSourceSectionFromATerm(t), keyClass, mapNode);
 	}
 
-	public TypedMapKeys(SourceSection source, String keylistClass) {
+	public TypedMapKeys(SourceSection source, String keyClass) {
 		super(source);
-		this.keyListClass = keylistClass;
+		this.keyClass = keyClass;
+		this.keyArrayClass = "[L" + keyClass + ";";
 	}
 
 	@Override
 	public abstract Object executeGeneric(VirtualFrame frame);
 
 	@Specialization
-	public IListTerm<?> doCached(@SuppressWarnings("rawtypes") PersistentMap map,
-			@SuppressWarnings("rawtypes") @Cached("getListClassConstructor()") Constructor<IListTerm> constr) {
-		return mkList(constr, map);
-
+	public Object doKeys(@SuppressWarnings("rawtypes") PersistentMap map, @Cached("getKeyClass()") Class<?> keyClass,
+			@Cached("getListInit(keyClass)") ITermInit listInit) {
+		@SuppressWarnings("unchecked")
+		Object[] keyArray = map.keySet().toArray((Object[]) Array.newInstance(keyClass, 0));
+		return listInit.apply(keyArray);
 	}
 
-	@TruffleBoundary
-	// FIXME: reflection is really bad for speed.
-	private IListTerm<?> mkList(@SuppressWarnings("rawtypes") Constructor<IListTerm> constr,
-			@SuppressWarnings("rawtypes") PersistentMap map) {
+	protected Class<?> getKeyClass() {
 		try {
-			return constr.newInstance(new Object[] { map.keySet() });
-		} catch (ReflectiveOperationException e) {
-			throw new InterpreterException("Could not instantiate list of keys", e);
+			return Class.forName(this.keyClass);
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException(e);
 		}
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	protected Constructor<IListTerm> getListClassConstructor() {
-		try {
-			Class<IListTerm> listClass = (Class<IListTerm>) this.getClass().getClassLoader().loadClass(keyListClass);
-			Constructor<IListTerm> constructor = listClass.getConstructor(Collection.class);
-			if (constructor == null) {
-				throw new InterpreterException("Could not find suitable constructor for list class " + keyListClass);
-			}
-
-			return constructor;
-		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException e) {
-			throw new InterpreterException("Could not find suitable class for list class " + keyListClass, e);
-		}
+	protected ITermInit getListInit(Class<?> keyClass) {
+		ITermRegistry termReg = getContext().getTermRegistry();
+		return termReg.lookupClassConstructorWrapper(termReg.getListClass(keyClass));
 	}
+
+	// @Specialization
+	// public IListTerm<?> doCached(@SuppressWarnings("rawtypes") PersistentMap map,
+	// @SuppressWarnings("rawtypes") @Cached("getListClassConstructor()") Constructor<IListTerm> constr) {
+	// return mkList(constr, map);
+	//
+	// }
+	//
+	// @TruffleBoundary
+	// // FIXME: reflection is really bad for speed.
+	// private IListTerm<?> mkList(@SuppressWarnings("rawtypes") Constructor<IListTerm> constr,
+	// @SuppressWarnings("rawtypes") PersistentMap map) {
+	// try {
+	// return constr.newInstance(new Object[] { map.keySet() });
+	// } catch (ReflectiveOperationException e) {
+	// throw new InterpreterException("Could not instantiate list of keys", e);
+	// }
+	// }
+	//
+	// @SuppressWarnings({ "rawtypes", "unchecked" })
+	// protected Constructor<IListTerm> getListClassConstructor() {
+	// try {
+	// Class<IListTerm> listClass = (Class<IListTerm>) this.getClass().getClassLoader().loadClass(keyListClass);
+	// Constructor<IListTerm> constructor = listClass.getConstructor(Collection.class);
+	// if (constructor == null) {
+	// throw new InterpreterException("Could not find suitable constructor for list class " + keyListClass);
+	// }
+	//
+	// return constructor;
+	// } catch (ClassNotFoundException | NoSuchMethodException | SecurityException e) {
+	// throw new InterpreterException("Could not find suitable class for list class " + keyListClass, e);
+	// }
+	// }
 
 }
